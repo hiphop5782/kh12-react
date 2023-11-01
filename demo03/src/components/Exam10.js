a66a7b3bc37e6c19cf8b44103537f34183b2b9a2
hiphop5782 <zdlsz@hanmail.net>
2023-11-01 16:28:55 +0900

    수정 완료 및 취소 구현

--- demo03/src/components/Exam10.js
+++ demo03/src/components/Exam10.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Exam10 = ()=>{
     const [items, setItems] = useState([
@@ -13,6 +13,16 @@ const Exam10 = ()=>{
         {itemNo:9, itemName:"신라면", itemPrice:1500, itemType:"식품", edit:false},
         {itemNo:10, itemName:"하리보젤리", itemPrice:5500, itemType:"식품", edit:false}
     ]);
+    const [backup, setBackup] = useState([]);
+
+    //(중요) "시작하자마자" items의 내용을 backup으로 복제(1회)
+    useEffect(()=>{
+        setBackup(items.map(item=>{
+            const newItem = {...item};
+            return newItem;
+        }));
+    }, []);
+
 
     //줄을 수정상태로 변경하는 함수
     //- 이 함수를 실행하려면 최소한 itemNo는 알아야 한다
@@ -20,6 +30,7 @@ const Exam10 = ()=>{
     const changeToEdit = (target)=>{
         //console.log(target);
 
+        //아이템 변경
         const newItems = items.map(item=>{
             if(item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
                 return {
@@ -45,6 +56,55 @@ const Exam10 = ()=>{
             }
             return item;
         });
+        setItems(newItems);
+    };
+
+    //취소 버튼을 누른 경우 실행할 함수
+    //- backup에 들어있는 target과 번호가 같은 데이터를 items의 target과 같은 번호에 덮어쓰기
+    const cancelItem = (target) => {
+
+        //backup에서 target의 번호에 해당하는 객체를 찾는다 (filter)
+        const findResult = backup.filter(item=>item.itemNo === target.itemNo);
+        //console.log(findResult[0]);
+
+        //아이템 변경
+        const newItems = items.map(item=>{
+            if(item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                return {
+                    ...findResult[0],//다른건 백업데이터로 두고
+                    edit:false//edit를 false로 바꿔라
+                };
+            }
+            return item;//나머진 현상유지
+        });
+        
+        setItems(newItems);
+    };
+    const saveItem = (target) => {
+
+        //백업 데이터 중 target과 번호가 같은 데이터를 갱신
+        const newBackup = backup.map(item=>{
+            if(item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                return {
+                    ...target,//변경된 데이터로 저장하고
+                    edit:false//edit를 false로 바꿔라
+                };
+            }
+            return item;//나머진 현상유지
+        });
+        setBackup(newBackup);
+
+        //아이템 변경
+        const newItems = items.map(item=>{
+            if(item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                return {
+                    ...item,//다른건 그대로 둬도
+                    edit:false//edit를 false로 바꿔라
+                };
+            }
+            return item;//나머진 현상유지
+        });
+        
         setItems(newItems);
     };
 
@@ -94,8 +154,10 @@ const Exam10 = ()=>{
                                                         value={item.itemType} onChange={e=>changeItem(item, e)}/>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn-sm btn-secondary">취소</button>
-                                                    <button className="btn btn-sm btn-success ms-1">완료</button>
+                                                    <button className="btn btn-sm btn-secondary"
+                                                            onClick={e=>cancelItem(item)}>취소</button>
+                                                    <button className="btn btn-sm btn-success ms-1"
+                                                            onClick={e=>saveItem(item)}>완료</button>
                                                 </td>
                                             </tr>
                                         ) : (
