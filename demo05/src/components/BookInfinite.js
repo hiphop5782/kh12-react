7c8bc046dd8c6cb39ffad09bf7d8114f70a9f809
hiphop5782 <zdlsz@hanmail.net>
2023-11-07 16:10:42 +0900

    더보기 버튼으로 무한 스크롤을 구현

--- demo05/src/components/BookInfinite.js
+++ demo05/src/components/BookInfinite.js
@@ -19,7 +19,19 @@ const BookInfinite = (props)=>{
     };
     useEffect(()=>{
         loadBook();
-    }, []);
+    }, [page]);
+
+    //다음페이지
+    const nextPage = ()=>{
+        setPage(page+1);//페이지 1 증가
+    };
+
+    //개수가 변하면 페이지를 1로, 목록을 모두 지우고 다시 불러와야 한다
+    useEffect(()=>{
+        setPage(1);
+        setBookList([]);
+        //loadBook();
+    }, [size]);
 
     return (
         <>
@@ -30,10 +42,22 @@ const BookInfinite = (props)=>{
             </div>
 
             <div className="row mt-4">
+                <div className="col-2 offset-10">
+                    <select value={size} onChange={e=>setSize(e.target.value)}>
+                        <option value="20">20개씩 보기</option>
+                        <option value="30">30개씩 보기</option>
+                        <option value="50">50개씩 보기</option>
+                        <option value="100">100개씩 보기</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className="row mt-4">
                 <div className="col">
                     <table className="table">
                         <thead>
                             <tr>
+                                <th>번호</th>
                                 <th>제목</th>
                                 <th>저자</th>
                                 <th>출판사</th>
@@ -42,6 +66,7 @@ const BookInfinite = (props)=>{
                         <tbody>
                             {bookList.map(book=>(
                             <tr key={book.bookId}>
+                                <td>{book.bookId}</td>
                                 <td>{book.bookTitle}</td>
                                 <td>{book.bookAuthor}</td>
                                 <td>{book.bookPublisher}</td>
@@ -50,6 +75,18 @@ const BookInfinite = (props)=>{
                         </tbody>
                     </table>
                 </div>
+
+                {/* 더보기 버튼 */}
+                
+                <div className="row mt-2">
+                    <div className="col">
+                        <button className="btn btn-primary w-100" onClick={nextPage}>
+                            {size}개 더보기
+                        </button>
+                    </div>
+                </div>
+                
+                
             </div>
         </>
     );
